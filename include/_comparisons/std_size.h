b13670cd8ca7b60c825401f222719e79e81bd028
Serguei E. Leontiev <leo@sai.msu.ru>
2025-11-29 21:12:07 +0300

    Добавлен в таблицу std::size() и suncc

---
+++ include/_comparisons/std_size.h
@@ -0,0 +1,14 @@
+// vim:set sw=4 ts=8 et fileencoding=utf8:
+// SPDX-License-Identifier: BSD-2-Clause
+// SPDX-FileCopyrightText: 2025 Сергей Леонтьев (leo@sai.msu.ru)
+
+#ifndef STD_SIZE_H_270
+#define STD_SIZE_H_270
+
+#if defined(__cplusplus)
+    #include <vector>
+
+    #define std_size(a)  (std::size(a))
+#endif
+
+#endif  // STD_SIZE_H_270
