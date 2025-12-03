e74003b93ae67017db44d91fffbaf5da90a50cfa
Serguei E. Leontiev <leo@sai.msu.ru>
2025-12-03 09:25:55 +0300

    Change examples-build.bat for MSBuild

--- examples-build.bat
+++ examples-build.bat
@@ -13,27 +13,35 @@ if not "%1"=="" (
     )
     set platform=vs%VisualStudioVersion%-nmake
     set generator="NMake Makefiles"
-    set post_check=..\..\tests\check_logs\post_check.py
-    set jlt=Testing\Temporary\LastTest-junit.xml
 ) else (
     set platform=vs%VisualStudioVersion%
 )
 md build\%platform%
 cd build\%platform%
+set build_type=Release
+set jlt=Testing\Temporary\LastTest-junit.xml
+set post_check=..\..\tests\check_logs\post_check.py
+python "%post_check%" --selftest
+if not errorlevel 1 (
+    set pc=1
+) else (
+    set pc=0
+    echo ""
+    echo "No python, venv or modules, skip %post_check% %jlt%"
+    echo "See tests\check_log\venv.bat"
+)
 if "%1"=="nmake" (
     rem TODO XXX shift?
     cmake --preset default -G %generator% ..\.. %2 %3 %4 %5 %6 %7 %8 %9
     cmake --build .
     ctest --output-junit "%jlt%" -E post_check_
-    python "%post_check%" --selftest
-    if not errorlevel 1 (
-	python "%post_check%" "%jlt%"
-    ) else (
-	echo ""
-	echo "No python, venv or modules, skip %post_check% %jlt%"
-	echo "See tests\check_log\venv.bat"
-    )
 ) else (
-    cmake --preset default ..\.. %*
-    start .
+    rem cmake --preset default ..\.. %*
+    rem start .
+    cmake -B . -DCMAKE_CXX_COMPILER=cl -DCMAKE_C_COMPILER=cl -DCMAKE_BUILD_TYPE=%build_type% -S ..\.. --preset default
+    cmake --build . --config %build_type%
+    ctest --output-junit "%jlt%" --build-config %build_type%
+)
+if "%pc%"=="1" (
+    python "%post_check%" "%jlt%"
 )
