bfef29b5cba66251bdd9dced28e907c8181cae0f
Serguei E. Leontiev <leo@sai.msu.ru>
2025-12-03 03:35:50 +0300

    Preliminary workaround for FetchContent

--- examples-build.bat
+++ examples-build.bat
@@ -5,7 +5,7 @@ rem SPDX-FileCopyrightText: 2025 Сергей Леонтьев (leo@sai.msu.ru)
 
 if not "%1"=="" (
     if not "%1"=="nmake" (
-	echo Usage: example-build.bat [nmake]
+	echo Usage: example-build.bat [nmake] [cmake args...]
 	echo     nmake - If exist, then use "NMake Makefiles" cmake generator
 	echo .     
 	echo     Without argument - use default generator for Visual Studio
@@ -20,9 +20,10 @@ if not "%1"=="" (
 )
 md build\%platform%
 cd build\%platform%
-if not "%1"=="" (
-    cmake -G %generator% ..\..
-    nmake
+if "%1"=="nmake" (
+    rem TODO XXX shift?
+    cmake --preset default -G %generator% ..\.. %2 %3 %4 %5 %6 %7 %8 %9
+    cmake --build .
     ctest --output-junit "%jlt%" -E post_check_
     python "%post_check%" --selftest
     if not errorlevel 1 (
@@ -33,6 +34,6 @@ if not "%1"=="" (
 	echo "See tests\check_log\venv.bat"
     )
 ) else (
-    cmake ..\..
+    cmake --preset default ..\.. %*
     start .
 )
