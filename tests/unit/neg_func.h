40a32b87eb5f7c884c8c75b163df945b93c7215d
Serguei E. Leontiev <leo@sai.msu.ru>
2025-12-27 20:21:51 +0300

    MSVC bug

--- tests/unit/neg_func.h
+++ tests/unit/neg_func.h
@@ -1,9 +1,15 @@
 // vim:set sw=4 ts=8 et fileencoding=utf8:
 #define N  (7)
 #if !__cplusplus
-    static size_t nf_foo(int a[static N]) {
-        return TU_COUNTOF(a);
-    }
+    #if !_MSC_VER
+        static size_t nf_foo(int a[static N]) {
+            return TU_COUNTOF(a);
+        }
+    #else
+        static size_t nf_foo(int a[N]) {
+            return TU_COUNTOF(a);
+        }
+    #endif
 #else
     static size_t nf_foo(int a[N]) {
         return TU_COUNTOF(a);
