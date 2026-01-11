35792992b1d75a38686d0f921c15420aa0839cce
Serguei E. Leontiev <leo@sai.msu.ru>
2026-01-11 10:11:46 +0300

    Rename ALX_COUNTOF to N3369_NITEMS

--- tests/unit/comparisons/N3369_NITEMS_expected.cmake
+++ tests/unit/comparisons/N3369_NITEMS_expected.cmake
@@ -6,20 +6,20 @@ if (NOT (HAVE_BUILTIN_TYPES_COMPATIBLE_P AND HAVE_TYPEOF))
     return ()
 endif ()
 
-try_compile(HAVE_ALX_COUNTOF
+try_compile(HAVE_N3369_NITEMS
                 SOURCE_FROM_CONTENT builtin_types_compatible_p.c [=[
             #include <assert.h>
             #if __has_builtin(__builtin_types_compatible_p)
-                // ALX_COUNTOF() use `static_assert()`, not `_Static_assert()`
+                // N3369_NITEMS() use `static_assert()`, not `_Static_assert()`
                 static_assert(1);
                 int main(void) {}
             #endif
             ]=])
-if (NOT HAVE_ALX_COUNTOF)
-    message("Don't HAVE_ALX_COUNTOF")
+if (NOT HAVE_N3369_NITEMS)
+    message("Don't HAVE_N3369_NITEMS")
 endif ()
 
-function (tu_alx_countof_expected expected pos_pos neg_pos)
+function (tu_n3369_nitems_expected expected pos_pos neg_pos)
     set(pos_base "")
     foreach (b IN ITEMS ${pos_pos})
         if (b MATCHES "_vla.*_[n0]0")
@@ -112,9 +112,9 @@ function (tu_alx_countof_expected expected pos_pos neg_pos)
     set(${expected} "${pos_base};${neg_base}" PARENT_SCOPE)
 endfunction ()
 
-tu_alx_countof_expected(tu_alx_countof_available
-                           "${tu_pos_pos}" "${tu_neg_pos}")
-set(tu_alx_countof_params FALSE
-                          "ALX_COUNTOF" "_comparisons/ALX_COUNTOF.h"
-                          tu_alx_countof_available)
-list(APPEND tu_params_list tu_alx_countof_params)
+tu_n3369_nitems_expected(tu_n3369_nitems_available
+                         "${tu_pos_pos}" "${tu_neg_pos}")
+set(tu_n3369_nitems_params FALSE
+                          "N3369_NITEMS" "_comparisons/N3369_NITEMS.h"
+                          tu_n3369_nitems_available)
+list(APPEND tu_params_list tu_n3369_nitems_params)
