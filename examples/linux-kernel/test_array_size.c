3808b50cda9c45ef93e080ff2fb9ed99a929f360
Serguei E. Leontiev <leo@sai.msu.ru>
2026-02-24 01:54:37 +0300

    Added complicated test cases

--- examples/linux-kernel/test_array_size.c
+++ examples/linux-kernel/test_array_size.c
@@ -9,9 +9,20 @@
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Serguei E. Leontiev (leo@sai.msu.ru)");
 MODULE_DESCRIPTION("Test ARRAY_SIZE fail");
-MODULE_VERSION("0.4");
+MODULE_VERSION("0.5");
 
 // Usage:
+//
+// Test fail for int work_int(size_t m, size_t n, int (*arr)[m][n]):
+// $ make
+// $ sudo insmod test_array_size.ko test=10
+// $ sudo dmesg | tail -70
+// $ sudo reboot
+//
+// Test fail for size_t work_rec(size_t m, size_t n, struct rec (*arr)[m][n]):
+// $ sudo insmod test_array_size.ko test=20
+// $ sudo dmesg | tail -70
+// $ sudo reboot
 //
 // Test fail for multidimensional VLA:
 // $ make
@@ -29,6 +40,8 @@ MODULE_VERSION("0.4");
 // $ make PROPOSAL_LINUX_ARRAY_SIZE=1
 // $ sudo insmod test_array_size.ko test=0
 // $ sudo -s sh -c 'echo -10 > /sys/module/test_array_size/parameters/test'
+// $ sudo -s sh -c 'echo 10 > /sys/module/test_array_size/parameters/test'
+// $ sudo -s sh -c 'echo 20 > /sys/module/test_array_size/parameters/test'
 
 #ifdef PROPOSAL_LINUX_ARRAY_SIZE
 	#undef ARRAY_SIZE
@@ -43,20 +56,73 @@ MODULE_VERSION("0.4");
 	#define TAS_VARIANT  ("")
 #endif
 
+static int work_int(size_t m, size_t n, int (*arr)[m][n])
+{
+	// Need `-O0` for test fail
+	// For `-O` or above need three dimensions
+	int ret = 0;
+	for (size_t i = 0; i < ARRAY_SIZE(*arr); i++) {
+		for (size_t j = 0; j < ARRAY_SIZE((*arr)[0]); j++) {
+			ret += (*arr)[i][j];
+		}
+	}
+	return ret;
+}
+
+struct rec {
+	#ifdef REC_A
+		size_t a;
+	#endif
+	#ifdef REC_B
+		size_t b;
+	#endif
+};
+
+static size_t work_rec(size_t m, size_t n, struct rec (*arr)[m][n])
+{
+	size_t ret = 0;
+	for (size_t j = 0; j < ARRAY_SIZE((*arr)[0]); j++) {
+		for (size_t i = 0; i < ARRAY_SIZE(*arr); i++) {
+			#ifdef REC_A
+				ret += (*arr)[i][j].a;
+			#endif
+			#ifdef REC_B
+				ret += (*arr)[i][j].b;
+			#endif
+			ret++;
+		}
+	}
+	return ret;
+}
+
 static int tas_test_set_int(const char *val, const struct kernel_param *kp)
 {
 	int *pv = kp->arg;
 	int res = param_set_int(val, kp);
 	if (0 == res) {
 		pr_info("%s: *pv=%d %s\n", __func__, *pv, TAS_VARIANT);
-		if (0 < *pv) {
+		if (30 <= *pv) {
 			pr_info("%s: skip\n", __func__);
+		} else if (20 <= *pv) {
+			#define RM1917  (1917)
+			#define RN10  (10)
+			struct rec recs[RM1917][RN10] = {};
+			pr_info("work_rec(RM1917, RN10, &recs) = %zu\n",
+				work_rec(RM1917, RN10, &recs));
+		} else if (10 <= *pv) {
+			size_t im = 25;
+			size_t in = *pv - 10;
+			int (*ints)[im][in] = kzalloc(sizeof(*ints),
+						      GFP_KERNEL);
+			pr_info("work_int(im, in, ints) = %d\n",
+				work_int(im, in, ints));
+			kfree(ints);
 		} else if (-10 >= *pv) {
 			struct {} a[0];
 			pr_info("%zu, %zu, ARRAY_SIZE(a)=%zu\n",
 				sizeof(a), sizeof(a[0]), ARRAY_SIZE(a));
 		} else {
-			int v[*pv][*pv][*pv];
+			int v[*pv][*pv][*pv];  // With `-O0` may two dimensions
 			pr_info("%zu, %zu, ARRAY_SIZE(v)=%zu\n",
 				sizeof(v), sizeof(v[0]), ARRAY_SIZE(v));
 		}
