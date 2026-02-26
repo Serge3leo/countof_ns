1f446d3fed97b481faea99bfc3290614b1522811
Serguei E. Leontiev <leo@sai.msu.ru>
2026-02-26 18:05:04 +0300

    OrangeC CI (#69)
    * Serge3leo/orangec-msys2@v0 (TODO: interface)
    * ilammy/msvc-dev-cmd@v1
    * Check ERRORLEVEL on Windows
    * Manual start short_example, without CTest

--- examples-build.bat
+++ examples-build.bat
@@ -1,29 +1,28 @@
-rem @echo off
+@echo off
 rem vim:set sw=4 ts=8 et fileencoding=utf8:
 rem SPDX-License-Identifier: BSD-2-Clause
 rem SPDX-FileCopyrightText: 2025 Сергей Леонтьев (leo@sai.msu.ru)
 
 set cc=cl
 set cxx=cl
-if "%1"=="nmake" (
+set platform=vs%VisualStudioVersion%
+set generator=Visual Studio xx yyyy
+if "%1"=="%cc%" (
     rem TODO shift
-    set platform=vs%VisualStudioVersion%-nmake
-    set generator=NMake Makefiles
 ) else if "%1"=="--" (
     rem TODO shift
-    set platform=vs%VisualStudioVersion%
-    set generator=Visual Studio xx yyyy
 ) else if "%1"=="" (
-    set platform=vs%VisualStudioVersion%
-    set generator=Visual Studio xx yyyy
-) else if "%1"=="occ" (
     rem TODO shift
-    set platform=occ
-    set generator=MSYS Makefiles
+) else if "%1"=="nmake" (
+    set platform=vs%VisualStudioVersion%-nmake
+    set generator=NMake Makefiles
+) else if "%1"=="occ" (
     set cc=occ
     set cxx=occ
+    set platform=occ
+    set generator=MSYS Makefiles
 ) else (
-    echo "Usage: example-build.bat [[nmake|--|occ] [cmake args...]]"
+    echo "Usage: example-build.bat [[--|cl|nmake|occ] [cmake args...]]"
     echo     nmake - If exist, then use "NMake Makefiles" cmake generator
     echo     occ - Use OrangeC with "MSYS Makefiles" cmake generator
     echo .
@@ -51,12 +50,18 @@ if not "%generator:~0,6%"=="Visual" (
     cmake -B . -DCMAKE_CXX_COMPILER=%cxx% -DCMAKE_C_COMPILER=%cc% ^
           -G "%generator%" ^
           -S ..\.. --preset %prst% %2 %3 %4 %5 %6 %7 %8 %9
+    if errorlevel 1 exit /b
     cmake --build .
+    if errorlevel 1 exit /b
     ctest --output-on-failure %CTEST_ARGS%
+    if errorlevel 1 exit /b
 ) else (
     cmake -B . -DCMAKE_CXX_COMPILER=%cxx% -DCMAKE_C_COMPILER=%cc% ^
           -G "%generator%" ^
           -DCMAKE_BUILD_TYPE=%build_type% -S ..\.. --preset %prst% %2 %3 %4 %5 %6 %7 %8 %9
+    if errorlevel 1 exit /b
     cmake --build . --config %build_type%
+    if errorlevel 1 exit /b
     ctest --output-on-failure --build-config %build_type% %CTEST_ARGS%
+    if errorlevel 1 exit /b
 )
